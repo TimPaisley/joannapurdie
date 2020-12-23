9d9d7b4b13656bd2822fdfb26a72b4fb6a629aac
Tim Paisley <tepaisley@gmail.com>
2020-12-23 15:35:56 +1300

    Add Christmas card

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ var elems = document.getElementsByClassName('no-cache');
 for (var i = 0; i < elems.length; i++) {
     elems[i].attributes['src'].value += "?a=" + Math.random();
 }
+
+function swapImage() {
+    var card = document.getElementById("card");
+    if (card.src.endsWith("christmas-title.jpg")) {
+        card.src = "images/christmas-boons.jpg";
+    } else {
+        card.src = "images/christmas-title.jpg";
+    }
+}
